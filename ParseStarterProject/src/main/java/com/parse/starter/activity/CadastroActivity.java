c20687c10bb42136cde01f92c64c29b5d38bc1a2
Felipe <ramos.felippe@gmail.com>
2018-01-13 15:54:27 -0200

    -Implementação do login -Criação da tela principal: configuração da toolbar -Implementação do método deslogar

--- ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/CadastroActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -19,6 +20,7 @@ public class CadastroActivity extends AppCompatActivity {
     private EditText textoUsuario;
     private EditText textoEmail;
     private EditText textoSenha;
+    private TextView facaLogin;
     private Button botaoCadastrar;
 
     @Override
@@ -29,12 +31,19 @@ public class CadastroActivity extends AppCompatActivity {
         textoUsuario = (EditText) findViewById(R.id.text_usuario);
         textoEmail = (EditText) findViewById(R.id.text_email);
         textoSenha = (EditText) findViewById(R.id.text_senha);
+        facaLogin = (TextView) findViewById(R.id.text_faca_login);
         botaoCadastrar = (Button) findViewById(R.id.button_cadastrar);
 
         botaoCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 cadastrarUsuario();
+            }
+        });
+        facaLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirLoginUsuario();
             }
         });
     }
@@ -63,9 +72,10 @@ public class CadastroActivity extends AppCompatActivity {
         });
     }
 
-    public void abrirLoginUsuario(View view){
+    public void abrirLoginUsuario(){
         Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
         startActivity(intent);
+        finish();
     }
 
 
--- ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
@@ -8,9 +8,14 @@
  */
 package com.parse.starter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -27,14 +32,52 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbarPrincipal;
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+      //Configurar toolbar
+      toolbarPrincipal = (Toolbar) findViewById(R.id.toolbar_principal);
+      toolbarPrincipal.setLogo(R.drawable.instagramlogo);
+      setSupportActionBar(toolbarPrincipal);
+
   }
 
-  private void metodosParse(){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.action_configuracoes:
+                return true;
+            case R.id.action_compartilhar:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deslogarUsuario(){
+        ParseUser.logOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void metodosParse(){
 
       //Testes de Métodos usados no Parse
 
